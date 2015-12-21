d0c811ac16811ff1f7a2237d9683e243a15f111f
Piere Wöhl <piere@pierewoehl.de>
2015-12-21 17:35:54 +0100

    Setup A Bit

--- DNS-Installer-Debian.sh
+++ DNS-Installer-Debian.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+read -p "First of all the New Hostname" Hostname
+hostnamectl set-hostname $HOSTNAME
 echo "Going to Install Software"
+	echo "Installing Installation Helper Software"
+		apt-get install lynx -y
 	echo "Installing PowerDNS..."
 		apt-get install pdns-server pdns-backend-mysql -y
 	echo "Installing MySQL..."
@@ -18,21 +22,67 @@ echo "Going to Install Software"
 		apt-key add /tmp/webmin.asc
 		apt-get update
 		apt-get install webmin
-echo "Going over to Configuration the Software"
+echo "Going over to Configure the Software"
 	echo "Configure PowerDNS..."
 		sed 's/# recursor/recursor=8.8.8.8/g' /etc/powerdns/pdns.conf
 	echo "Configure MySQL Replication..."
+		echo "Sync Database..."
+			scp service@pdns1.fflin.link:/STORAGE/mysqlbackup/latest.sql /tmp/latest.sql
+			echo "Login to your MySQL Servers Root Account:"
+			read -p "ROOT Password: " MySQLPW
+			mysql -u root -p $MySQLPW < cat /tmp/latest.sql
+		
+		echo "Adding Service User to Local MySQL Server"
+			read -p "Choose Password for Service Account: " MySQLServicePW
+			mysql -u root -p $MySQLPW -c "CREATE USER 'service'@'localhost' IDENTIFIED BY '"$MySQLServicePW"';"
+			mysql -u root -p $MySQLPW -c "GRANT USAGE ON *.* TO 'service'@'localhost';"
+		
+		echo "Enforce Policies and Rights for Service User"
+			mysql -u root -p $MySQLPW -c  "GRANT SELECT, EXECUTE, SHOW VIEW, ALTER, ALTER ROUTINE, CREATE, CREATE ROUTINE, CREATE TEMPORARY TABLES, CREATE VIEW, DELETE, DROP, EVENT, INDEX, INSERT, REFERENCES, TRIGGER, UPDATE, LOCK TABLES  ON `pdns`.* TO 'service'@'localhost' WITH GRANT OPTION;"
+			mysql -u root -p $MySQLPW -c "FLUSH PRIVILEGES;"
+		
 	echo "Configure PowerAdmin..."
-		echo "Doing Installation on Shell"
 		echo "Creating Webserver Configuration..."
+			echo "<VirtualHost *:8080>" >> /etc/apache2/sites-available/poweradmin.conf
+			echo "DocumentRoot /var/www/poweradmin" >> /etc/apache2/sites-available/poweradmin.conf
+			echo "</VirtualHost>" >> /etc/apache2/sites-available/poweradmin.conf
+		echo "Doing Installation on Shell"
+			lynx http://localhost:8080 -accept_all_cookies
 	echo "Configure Webmin..."
 		echo "Loading Bootstrap Theme"
+			wget http://theme.winfuture.it/bwtheme.wbt.gz -O /tmp/bwtheme.wbt.tgz
+			cd /tmp/
+			tar xvf bwtheme.wbt.tgz
+			mv bootstrap /usr/share/webmin/
+			sed 's/theme=?/theme=bootstrap/g' /etc/webmin/config -i
 		echo "Disable Autostart"
 			systemctl disable webmin
-	echo "Configure SSL Keys..."
-echo "Going over to Configure the System"
-	echo "Set up User for MySQL Replication..."
-	echo "Configure SSH and SSH Tunnel for MySQL Replication..."
-echo "Enforce Security Policies"
 echo "Going to register this DNS Server to Master DNS Server"
+	echo "Connecting to Master..."
+		echo "Add Server to Zone"
+		clear
+		mapfile -t lines < <(ip addr show | grep inet | grep eth0 | awk {'print $2'} | cut -d'/' -f1)
+		
+		for (( i=0; i < ${#lines[@]} ; i++ )); do 
+			printf "%s: %s\n" "$i" "${lines[i]}"
+		done
+		read -p "Choose IPv4 Address to Use for A-Record: " IPv4
+		IPv4=${lines[$IPv4]}
+		
+		clear
+		mapfile -t lines < <(ip addr show | grep inet6 | grep global | awk {'print $2'} | cut -d'/' -f1)
+		for (( i=0; i < ${#lines[@]} ; i++ )); do 
+			printf "%s: %s\n" "$i" "${lines[i]}"
+		done
+		read -p "Choose IPv6 Address to Use for AAAA-Record: " IPv6
+		IPv6=${lines[$IPv6]}
+		
+		echo "A-Record: " $IPv4
+		echo "AAAA-Record: " $IPv6
+		echo "Hostname: " $HOSTNAME
+		
+		mysql -u root -p $MySQLPW -c "INSERT INTO records (domain_id,name,type,content,ttl,disabled,auth) VALUES (2,'"$HOSTNAME"','A','"$IPv4"',86400,0,1)" pdns
+		mysql -u root -p $MySQLPW -c "INSERT INTO records (domain_id,name,type,content,ttl,disabled,auth) VALUES (2,'"$HOSTNAME"','AAAA','"$IPv6"',86400,0,1)" pdns
+		unset MySQLPW
+		unset MySQLServicePW
 	
